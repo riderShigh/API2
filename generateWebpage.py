64859ccf3b66b4266711d6720d601ea793f23e48
riderShigh <eltoncyho@gmail.com>
2015-08-29 14:47:14 -0700

    patch

--- generateWebpage.py
+++ generateWebpage.py
@@ -7,7 +7,7 @@ baseText = fBaseHtml.read();
 mageList = json.loads(open('mages.json').read());
 
 for key,value in mageList.iteritems():
-	writeText = baseText.replace('Katarina',value);
+	#writeText = baseText.replace('Katarina',value);
 	champName = value.replace(" ","");
 	champName = champName.replace("'","");
 	champName = champName.lower();
@@ -17,6 +17,8 @@ for key,value in mageList.iteritems():
 	if champName == 'Kogmaw': champName = 'KogMaw';
 	if champName == 'Twistedfate': champName = 'TwistedFate';
 	print champName;
+
+	writeText = baseText.replace('Katarina',champName);
 
 	writeText = writeText.replace('/1/champStats.json','/'+key+'/champStats.json');
 
